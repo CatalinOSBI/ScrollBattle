dd96a01493ff7ad88a902dad4470f5f032f7572e
CatalinOSBI <garjoaba_c@yahoo.com>
2024-09-18 16:33:52 +0300

    menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import "./home.css";
 
 export default function Home() {
   return (
-    <p>TEST</p>
+    <main className="flex items-center justify-center h-lvh">
+      <div className="w-2/5 border-2 border-white max-w-md min-w-80 flex items-center justify-center h-2/4 menu" >
+        <p>Scroll Battle</p>
+      </div>
+    </main>
   );
 }
