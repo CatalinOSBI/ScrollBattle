cc4ac504b2fa6d5faee240c83d60acf0f4a394cf
CatalinOSBI <garjoaba_c@yahoo.com>
2024-09-23 14:48:31 +0300

    background + firebase init.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,18 +14,30 @@ const noodleFont = localFont({
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-lvh background">
-      <div className="w-2/5 border gap-4 rounded-md max-w-md min-w-[21.5rem] flex flex-col items-center justify-center h-2/4 menu">
-        <div className="flex flex-col items-end mb-[10rem]">
-          <p className={`${boldFont.className} text-8xl`}>
-            <span className="italic">Scr</span>oll
-          </p>
-          <p className={`${noodleFont.className} text-7xl leading-[3.5rem]`}>
-            Battle
-          </p>
+    <main className="stars">
+      <div className="flex items-center justify-center h-lvh background">
+        <div className="w-2/5 border gap-4 rounded-md max-w-md min-w-[21.5rem] flex flex-col items-center justify-center h-2/4 menu">
+          <div className="flex flex-col items-end mb-[10rem]">
+            <p className={`${boldFont.className} text-8xl`}>
+              <span className="italic">Scr</span>oll
+            </p>
+            <p className={`${noodleFont.className} text-7xl leading-[3.5rem]`}>
+              Battle
+            </p>
+          </div>
+          <OSBIbutton
+            buttonName="PLAY"
+            width={248}
+            primColor="rgb(53 53 53) 0%"
+            secColor="rgb(77 77 77 / 93%) 100%"
+          />
+          <OSBIbutton
+            buttonName="SIGN UP"
+            width={248}
+            primColor="rgb(53 53 53) 0%"
+            secColor="rgb(77 77 77 / 93%) 100%"
+          />
         </div>
-        <OSBIbutton buttonName="PLAY" width={248} primColor="rgb(53 53 53) 0%" secColor="rgb(77 77 77 / 93%) 100%" />
-        <OSBIbutton buttonName="SIGN UP" width={248} primColor="rgb(53 53 53) 0%" secColor="rgb(77 77 77 / 93%) 100%" />
       </div>
     </main>
   );
