cdffd7d61f98205336598fa7eaa3af44b06c39f9
CatalinOSBI <garjoaba_c@yahoo.com>
2024-09-27 18:45:22 +0300

    auth

--- src/app/Login/AuthContext.tsx
+++ src/app/Login/AuthContext.tsx
@@ -1,29 +1,42 @@
 "use client";
 
+import { initializeApp } from "firebase/app";
+import { FirebaseApp } from "firebase/app"; // firebase typescript
 import React, {
   createContext,
   useContext,
   useState,
+  useEffect,
   useRef,
   ReactNode,
   RefObject,
 } from "react";
-import { initializeApp } from "firebase/app";
-import { FirebaseApp } from "firebase/app"; // firebase typescript
 import {
   getAuth,
   connectAuthEmulator,
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
   sendEmailVerification,
-  sendSignInLinkToEmail,
+  GoogleAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
 
 //Interface for the context values/functions
 interface authContextInterface {
-  value: string;
+  isLoggedIn: boolean;
   emailRef: RefObject<HTMLInputElement>;
   passwordRef: RefObject<HTMLInputElement>;
+  emailLogRef: RefObject<HTMLInputElement>;
+  passwordLogRef: RefObject<HTMLInputElement>;
+  userEmail: string | null;
+  handleSignOut: () => void;
   handleSignUp: (
+    email: string,
+    password: string,
+    e: React.FormEvent<HTMLFormElement>
+  ) => Promise<void>;
+  handleLogIn: (
     email: string,
     password: string,
     e: React.FormEvent<HTMLFormElement>
@@ -41,18 +54,27 @@ export const AuthContext = createContext<authContextInterface | undefined>(
 
 //Provider component
 export const AuthProvider: React.FC<authProviderProps> = ({ children }) => {
-  const [value, setTheme] = useState("light");
+  const [userEmail, setUserEmail] = useState<string | null>(null);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+  const emailLogRef = useRef<HTMLInputElement>(null);
+  const passwordLogRef = useRef<HTMLInputElement>(null);
 
   const firebaseConfig = {
-
+    apiKey: process.env.NEXT_PUBLIC_FIRE1,
+    authDomain: process.env.NEXT_PUBLIC_FIRE2,
+    projectId: process.env.NEXT_PUBLIC_FIRE3,
+    storageBucket: process.env.NEXT_PUBLIC_FIRE4,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIRE5,
+    appId: process.env.NEXT_PUBLIC_FIRE6,
+    measurementId: process.env.NEXT_PUBLIC_FIRE7,
   };
 
   // Initialize Firebase
   const app: FirebaseApp = initializeApp(firebaseConfig);
   const auth = getAuth(app);
-  connectAuthEmulator(auth, "http://localhost:9099");
+  // connectAuthEmulator(auth, "http://localhost:9099");
 
   //Sing Up (Create Account)
   const handleSignUp = (
@@ -61,11 +83,18 @@ export const AuthProvider: React.FC<authProviderProps> = ({ children }) => {
     e: React.FormEvent<HTMLFormElement>
   ): Promise<void> => {
     e.preventDefault();
+
+    // Check for empty fields
+    if (!email || !password) {
+      return Promise.reject("Password and email are required"); //Break
+    }
+
     return createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user!;
         console.log(user);
+
         // sendEmailVerification(user);
         console.log("User signed in successfully!");
       })
@@ -76,9 +105,70 @@ export const AuthProvider: React.FC<authProviderProps> = ({ children }) => {
       });
   };
 
+  //Log In
+  const handleLogIn = (
+    email: string,
+    password: string,
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault();
+    return signInWithEmailAndPassword(auth, email, password)
+    .then(
+      (userCredential) => {
+        // Signed in
+        const user = userCredential.user!;
+        console.log("User logged in successfully!");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorCode, errorMessage);
+      });  
+  };
+
+  //Sign Out
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        window.location.reload();
+        //Signed out
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorCode, errorMessage);
+      });
+  };
+
+  //Check when user logs in/out
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+
+        //Get user data
+        setUserEmail(user.email);
+      } else {
+        setIsLoggedIn(false);
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <AuthContext.Provider
-      value={{ value, emailRef, passwordRef, handleSignUp }}
+      value={{
+        isLoggedIn,
+        emailRef,
+        passwordRef,
+        emailLogRef,
+        passwordLogRef,
+        userEmail,
+        handleSignUp,
+        handleSignOut,
+        handleLogIn,
+      }}
     >
       {children}
     </AuthContext.Provider>
--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,25 +1,78 @@
-'use client'
+"use client";
 
 import React from "react";
-import { useAuth } from "./AuthContext"; 
+import { useAuth } from "./AuthContext";
 
 const Page = () => {
-  const { emailRef, passwordRef } = useAuth(); 
-
-  const handleTest = (e:React.FormEvent<HTMLFormElement>) => { 
-    e.preventDefault();
-    console.log(emailRef.current!.value)
-    console.log(passwordRef.current!.value)
-   }
+  const {
+    emailLogRef,
+    passwordLogRef,
+    emailRef,
+    passwordRef,
+    userEmail,
+    handleSignUp,
+    handleSignOut,
+    handleLogIn,
+  } = useAuth();
 
   return (
-    <form name="formA">
-      <input ref={emailRef} placeholder="email" type="text" className="bg-black border" />
-      <input ref={passwordRef} placeholder="password" type="text" className="bg-black border" />
-      <button className="border" onClick={(e)=>handleTest(e)}>
-        Sign Up
+    <>
+      {/* Sign Up */}
+      <form
+        name="formA"
+        onSubmit={(e) =>
+          handleSignUp(emailRef.current!.value, passwordRef.current!.value, e)
+        }
+      >
+        <input
+          ref={emailRef}
+          placeholder="email"
+          type="text"
+          className="bg-black border"
+        />
+        <input
+          ref={passwordRef}
+          placeholder="password"
+          type="text"
+          className="bg-black border"
+        />
+        <button className="border" type="submit">
+          Sign Up
+        </button>
+      </form>
+
+      {/* Log In */}
+      <form
+        name="formB"
+        onSubmit={(e) =>
+          handleLogIn(emailLogRef.current!.value, passwordLogRef.current!.value, e)
+        }
+      >
+        <input
+          ref={emailLogRef}
+          placeholder="email"
+          type="text"
+          className="bg-black border"
+        />
+        <input
+          ref={passwordLogRef}
+          placeholder="password"
+          type="text"
+          className="bg-black border"
+        />
+        <button className="border" type="submit">
+          Log 
+        </button>
+      </form>
+
+      {/* Sign Out */}
+      <button className="border" onClick={handleSignOut}>
+        Sign Out
       </button>
-    </form>
+      <button className="border" onClick={() => console.log(userEmail)}>
+        log
+      </button>
+    </>
   );
 };
 
